cfcf5065a6b0aa836a8b8c1839135781acdeb544
Zmohseny <zeinabemohseni@gmail.com>
2024-02-05 23:15:48 +0330

    set name for register

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,25 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import CustomInput from "./user/test";
 import apiRequests from "@/Axios/config";
 import { useForm } from "react-hook-form";
+import { number } from "yup";
+import toast, { Toaster } from "react-hot-toast";
 function Contact({ placeholder, style, label }: any) {
   const { register, handleSubmit } = useForm();
-
+  const [name,setName]=useState<any>()
+  const [phone,setPhone]=useState<any>()
+  const [token, setToken] = useState<any>();
+  useEffect(() => {
+    getData(), setToken(localStorage.getItem("token"));
+  }, []);
   const onSubmit = () => {
     const token = localStorage.getItem("token");
-    apiRequests.post(
-      `/api/user/new-comment`,
-      {
-        comment: "text",
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
+    if (token) {
+      console.log("first")
+      apiRequests.post(
+        `/api/user/new-comment`,
+        {
+          comment: "text",
         },
-      }
-    ).then((res)=>console.log(res))
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      ).then((res)=>console.log(res)).catch(()=>toast.error("اپتدا ثبت نام "))
+    }
+    else{toast.error("اپتدا ثبت نام "),console.log("second")}
+  };
+  const getData = () => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      apiRequests
+        .get(`/api/user-document`, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((response) =>{
+        setName(
+            response.data.data[0].first_name+'   '+response.data.data[0].last_name 
+
+          ) 
+          setPhone(response.data.data[0].phone_number),console.log(response)}
+        );
+    }
   };
   return (
     <div className="mt-[8%] flex flex-col justify-center  items-center w-[70%] mx-auto px-5">
@@ -27,25 +54,29 @@ function Contact({ placeholder, style, label }: any) {
         شما می توانید در این قسمت با ما در تماس باشید
       </p>
 
-      <div className="flex w-[100%] justify-between items-center  flex-col sm:flex-row">
+    <div className="flex w-[100%] justify-between items-center  flex-col sm:flex-row">
         <CustomInput
           label="نام و نام خانوادگی"
           placeholder="مثال : امیر رئیسی"
           style="w-[100%] sm:w-[50%] "
           {...register("name")}
+          value={name}
         />
         <CustomInput
           label="شماره تلفن"
           placeholder="09223620356 :مثال"
           style="w-[100%] sm:w-[50%] sm:mr-[3%] mt-[2%] sm:mt-[0px] "
           {...register("number")}
+          value={phone}
+
         />
       </div>
 
-      <textarea
+     <textarea 
         className=" text-right w-[100%] h-[220px] border-[1px]  rounded-[4.73px] mt-[2%]"
         placeholder="پیام های خود را در اینجا بنویسید..."
         {...register("text")}
+        
       />
       <button
         className="w-[248px] h-[66px] border-[1px] border-[#288E87] rounded-[7.963px] mt-[2%] text-[#288E87]"
@@ -53,8 +84,10 @@ function Contact({ placeholder, style, label }: any) {
       >
         ثبت نظر
       </button>
+      <Toaster/>
     </div>
   );
+  
 }
 
 export default Contact;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,23 +5,30 @@ import Logo from "./Logo";
 import Link from "next/link";
 import apiRequests from "@/Axios/config";
 import { Hidden } from "@mui/material";
-
+import { register } from "module";
+ 
 function Navbar({ ref }: any) {
-  const [token,setToken]=useState<any>()
+  const [token, setToken] = useState<any>();
+  const [register, setRegister] = useState<any>();
+  useEffect(() => {
+    getData(), setToken(localStorage.getItem("token"));
+  }, []);
 
-useEffect(()=>{getData(), setToken(localStorage.getItem("token"))
-},[])
-
-  const getData=()=>{
-    const token=localStorage.getItem("token")
+  const getData = () => {
+    const token = localStorage.getItem("token");
     if (token) {
-      apiRequests.get(`/api/user-document`, { headers: { Authorization: `Bearer ${token}` } }).then((res)=>console.log(res))
-
+      apiRequests
+        .get(`/api/user-document`, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((response) =>
+          setRegister(
+            response.data.data[0].first_name+"   "+response.data.data[0].last_name
+          )
+        );
     }
-   
-
-  }
-  
+  };
+  // console.log(register)
   return (
     <nav className="  flex  justify-between  font-iranSans items-center  py-5  fixed z-10 backdrop-blur-sm  backdrop-filter shadow-2xl bg-black bg-opacity-25  w-full">
       <div className="  flex flex-col justify-center mr-[7.56%]">
@@ -40,83 +47,84 @@ useEffect(()=>{getData(), setToken(localStorage.getItem("token"))
       </div>
 
       <ul className="hidden lg:font-medium	 lg:flex lg:justify-center text-white  lg:gap-[1%] basis-[57%]">
-        <a href="#home"
-         
+        <a
+          href="#home"
           className="hover:bg-[#45CBC2] rounded-lg px-[1.4%] py-[1.5%] flex justify-center items-center "
         >
           خانه
         </a>
-        <a href="#satisfication"
-          
+        <a
+          href="#satisfication"
           className="hover:bg-[#45CBC2] rounded-lg px-[1.4%] py-[1.5%] flex justify-center items-center "
         >
           رضایت مراجعان
         </a>
-        <a href="#insurance"
-         
+        <a
+          href="#insurance"
           className="hover:bg-[#45CBC2] rounded-lg px-[1.4%] py-[1.5%] flex justify-center items-center "
         >
           بیمه های طرف قرارداد
         </a>
-        <a href="#contactUs"
-          
+        <a
+          href="#contactUs"
           className="hover:bg-[#45CBC2] rounded-lg px-[1.4%] py-[1.5%] flex justify-center items-center "
         >
           تماس با ما
         </a>
-        <a
-          
-          className="hover:bg-[#45CBC2] rounded-lg px-[1.4%] py-[1.5%] flex justify-center items-center "
-        >
+        <a className="hover:bg-[#45CBC2] rounded-lg px-[1.4%] py-[1.5%] flex justify-center items-center ">
           بلاگ
         </a>
         <Link
-          
           href="/login"
-          className={`hover:bg-[#45CBC2] rounded-lg px-[1.4%] py-[1.5%] flex justify-center items-center  ${token?"hidden":"flex"}`}
+          className={`hover:bg-[#45CBC2] rounded-lg px-[1.4%] py-[1.5%] flex justify-center items-center  ${
+            token ? "hidden" : "flex"
+          }`}
         >
           ورود / عضویت
         </Link>
       </ul>
-      {token?"salam": <div className="flex  items-center ml-[2%] lg:ml-[7.56%] ">
-        <Link
-          href="./login"
-          className="flex bg-[#45CBC2] w-[149px] h-[44px]  justify-center items-center text-white rounded-lg hover:bg-[#D6F3F1] hover:text-[#064247] group"
-        >
-          <div className="rounded-full bg-white hover:bg-[#45CBC2]  flex items-center group-hover:bg-[#45CBC2]">
-            <svg
-              className="z-10"
-              width="25"
-              height="24"
-              viewBox="0 0 25 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                className="group-hover:stroke-[#45CBC2]"
-                d="M12.5 22.25C6.84614 22.25 2.25 17.6539 2.25 12C2.25 6.34614 6.84614 1.75 12.5 1.75C18.1539 1.75 22.75 6.34614 22.75 12C22.75 17.6539 18.1539 22.25 12.5 22.25ZM12.5 2.25C7.12386 2.25 2.75 6.62386 2.75 12C2.75 17.3761 7.12386 21.75 12.5 21.75C17.8761 21.75 22.25 17.3761 22.25 12C22.25 6.62386 17.8761 2.25 12.5 2.25Z"
-                stroke="white"
-              />
-              <path
-                className=""
-                d="M16.5 12.25H8.5C8.36614 12.25 8.25 12.1339 8.25 12C8.25 11.8661 8.36614 11.75 8.5 11.75H16.5C16.6339 11.75 16.75 11.8661 16.75 12C16.75 12.1339 16.6339 12.25 16.5 12.25Z"
-                stroke="#064247"
-              />
-              <path
-                d="M12.5 16.25C12.3661 16.25 12.25 16.1339 12.25 16V8C12.25 7.86614 12.3661 7.75 12.5 7.75C12.6339 7.75 12.75 7.86614 12.75 8V16C12.75 16.1339 12.6339 16.25 12.5 16.25Z"
-                stroke="#064247"
-              />
-            </svg>
+      {token ? (
+        register
+      ) : (
+        <div className="flex  items-center ml-[2%] lg:ml-[7.56%] ">
+          <Link
+            href="./login"
+            className="flex bg-[#45CBC2] w-[149px] h-[44px]  justify-center items-center text-white rounded-lg hover:bg-[#D6F3F1] hover:text-[#064247] group"
+          >
+            <div className="rounded-full bg-white hover:bg-[#45CBC2]  flex items-center group-hover:bg-[#45CBC2]">
+              <svg
+                className="z-10"
+                width="25"
+                height="24"
+                viewBox="0 0 25 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  className="group-hover:stroke-[#45CBC2]"
+                  d="M12.5 22.25C6.84614 22.25 2.25 17.6539 2.25 12C2.25 6.34614 6.84614 1.75 12.5 1.75C18.1539 1.75 22.75 6.34614 22.75 12C22.75 17.6539 18.1539 22.25 12.5 22.25ZM12.5 2.25C7.12386 2.25 2.75 6.62386 2.75 12C2.75 17.3761 7.12386 21.75 12.5 21.75C17.8761 21.75 22.25 17.3761 22.25 12C22.25 6.62386 17.8761 2.25 12.5 2.25Z"
+                  stroke="white"
+                />
+                <path
+                  className=""
+                  d="M16.5 12.25H8.5C8.36614 12.25 8.25 12.1339 8.25 12C8.25 11.8661 8.36614 11.75 8.5 11.75H16.5C16.6339 11.75 16.75 11.8661 16.75 12C16.75 12.1339 16.6339 12.25 16.5 12.25Z"
+                  stroke="#064247"
+                />
+                <path
+                  d="M12.5 16.25C12.3661 16.25 12.25 16.1339 12.25 16V8C12.25 7.86614 12.3661 7.75 12.5 7.75C12.6339 7.75 12.75 7.86614 12.75 8V16C12.75 16.1339 12.6339 16.25 12.5 16.25Z"
+                  stroke="#064247"
+                />
+              </svg>
+            </div>
+            <span className="flex mr-[8px] font-iranSansLight">
+              درخواست نوبت
+            </span>
+          </Link>
+          <div className="lg:hidden">
+            <IoMenu style={{ fontSize: "2rem" }} />
           </div>
-          <span className="flex mr-[8px] font-iranSansLight">
-            درخواست نوبت{" "}
-          </span>
-        </Link>
-        <div className="lg:hidden">
-          <IoMenu style={{ fontSize: "2rem" }} />
         </div>
-      </div>}
-     
+      )}
     </nav>
   );
 }
